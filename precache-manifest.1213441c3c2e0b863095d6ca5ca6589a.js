9809b65f09dd7b90545e7eedf30a44f068df1e2f
Stanislav Obrazcoff <obrazcoff@gmail.com>
2020-07-24 13:05:41 +0200

    Updates

---
+++ precache-manifest.1213441c3c2e0b863095d6ca5ca6589a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22632be38ebf3f23607f98708c4120d8",
+    "url": "/basic-search/index.html"
+  },
+  {
+    "revision": "28de561857fe95c47700",
+    "url": "/basic-search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a99160ec45b0ee953924",
+    "url": "/basic-search/static/js/2.f9a0cbef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-search/static/js/2.f9a0cbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28de561857fe95c47700",
+    "url": "/basic-search/static/js/main.090edfe0.chunk.js"
+  },
+  {
+    "revision": "72c3b42ca39e5f40df95",
+    "url": "/basic-search/static/js/runtime-main.dd32a234.js"
+  }
+]);
